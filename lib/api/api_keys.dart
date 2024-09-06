369149f8d667364c9789315ff3c327926504a389
HowardLee134 <m_lee134@u.pacific.edu>
2024-09-06 00:01:12 -0700

    googleAPI Intergration

---
+++ lib/api/api_keys.dart
@@ -0,0 +1,1 @@
+const String googleAPIKey = 'AIzaSyBXfv04d83HpeiYt0lhy04rx5oOCJwY-8w';
---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
